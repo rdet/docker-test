196f4129c97f2e57973e255199e08e57c7c07d48
Ykrishna <you@example.com>
2018-12-05 14:21:52 +0000

    adding new changes

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
-<?php
-  // Declare the variable 'string' and assign it a value.
-  // The <br> is the HTML equivalent to a new line.
-  $string = 'Hello World!<br>';
-
-  // You can echo the variable, similar to the way you would echo a string.
-  echo $string;
-
-  // You could also use print.
-  print $string;
-
-  // Or, if you are familiar with C, printf can be used too.
-  printf('%s', $string);
-?>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
